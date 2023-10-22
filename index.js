9caf6d2afc0d91b11b189b4152a18183146ea698
Jay Sharma <jayshines98@gmail.com>
2023-10-22 23:52:47 +0530

    Product creation route function

--- index.js
+++ index.js
@@ -7,6 +7,51 @@ const port = 3000;
 
 app.use(bodyParser.json()); 
 
+const economicHeaders = {
+    headers: {
+      "x-appsecrettoken": process.env.ECONOMIC_API_KEY,
+      "x-agreementgranttoken ": process.env.ECONOMIC_AGREEMENT_GRANT_TOKEN,
+      "content-type": "application/json",
+    },
+  };
+
+  app.post("/product", (req, res) => {
+    try {
+      const shopifyNewProduct = req.body;
+      const { title: productName } = shopifyNewProduct;
+      let economicProduct = [];
+      shopifyNewProduct.variants.forEach((variant) => {
+        let temp = {
+          title: variant.title,
+          price: variant.price,
+          sku: variant.sku,
+        };
+        economicProduct.push(temp);
+      });
+  
+      economicProduct.forEach(async (newProduct) => {
+        const Product = {
+          productGroup: {
+            productGroupNumber: 1,
+          },
+          name: newProduct.title,
+          productNumber: newProduct.sku,
+          salesPrice: +newProduct.price,
+          description: productName,
+        };
+        await axios.post(
+          "https://restapi.e-conomic.com/products",
+          Product,
+          economicHeaders
+        );
+        console.log(Product);
+      });
+      res.send({ productName, economicProduct });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
   });
